c236de5eb99e7cefd9f0a74f485627532e9a5409
Kamau <kevinnjagi83@gmail.com>
2024-09-26 09:59:13 +0300

    logging and exception

--- setup.py
+++ setup.py
@@ -11,13 +11,14 @@ def get_requirements(file_path: str) -> List[str]:
     requirements = []
     with open(file_path) as file_object:
         requirements = file_object.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
-        print(f"Processed requirements: {requirements}")  # Add this
+        # Strip leading/trailing spaces and newlines from each requirement
+        requirements = [req.strip() for req in requirements if req.strip()]
 
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
 
     return requirements
+
 
 
 
