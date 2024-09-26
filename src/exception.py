f31f4a90b9d256d25eef96ddc6c58b116c41739e
Kamau <kevinnjagi83@gmail.com>
2024-09-26 12:05:47 +0300

    EDA & Problem statement

--- src/exception.py
+++ src/exception.py
@@ -1,10 +1,15 @@
-import sys 
-import logging
+import sys # access information about the current Python runtime, including exception details
+from src.logger import logging
 
 
-def error_message_detail(error, error_detail:sys):
+def error_message_detail(error, error_detail:sys): #error -> actual error that occurred
     _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
+    file_name=exc_tb.tb_frame.f_code.co_filename # extracts the filename from the traceback object where the error occurred
+    
+    # Create a formatted error message string, including:
+        # File name where the error occurred.
+        # Line number (exc_tb.tb_lineno) in the file where the error happened.
+        # Actual error message (str(error))
     error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         file_name,exc_tb.tb_lineno,str(error)
     )
@@ -12,13 +17,22 @@ def error_message_detail(error, error_detail:sys):
     
 
 
-class CustomException(Exception):
-    def __init__(self,error_message, error_detail:sys):
-        super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+class CustomException(Exception): # inherits from Python’s built-in Exception class
+    def __init__(self,error_message, error_detail:sys): # A simple description of the error, The system’s exception information
+        super().__init__(error_message) #  calls the base class (Exception) constructor to initialize the error_message
+        self.error_message=error_message_detail(error_message,error_detail=error_detail) # calls the error_message_detail() function to generate a detailed error message and assigns it to self.error_message
     
-    def __str__(self):
+    def __str__(self): # method allows the CustomException class to return the detailed error message when printed or logged
         return self.error_message
+    
+
+
+# This file defines a mechanism for creating 
+# and raising custom exceptions with detailed error messages, 
+# including information about the file and 
+# line number where the error occurred. 
+# The CustomException class can be used to provide more 
+# detailed error logging compared to a typical exception.
     
 
 
--- src/logger.py
+++ src/logger.py
@@ -1,17 +1,40 @@
-import logging
-import os
-from datetime import datetime
+import logging # for configuring and writing log messages
+import os # for creating directories and file paths
+from datetime import datetime # to generate the current timestamp for the log file name
 
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log" # generates a log file name based on the current date and time
+
+# Let's construct the full file path where the log file will be saved. It combines:
+    # The current working directory (os.getcwd()).
+    # A "logs" folder.
+    # The dynamically generated log file name.
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+
+# To ensure that the "logs" folder is created if it doesn’t exist. 
+# The exist_ok=True prevents an error if the folder already exists
 os.makedirs(logs_path,exist_ok=True)
 
+# Store the full path to the log file (including both the directory and file name) in LOG_FILE_PATH
 LOG_FILE_PATH=os.path.join(logs_path, LOG_FILE)
 
+
+# filename=LOG_FILE_PATH: Specifies that log messages will be written to the file defined by LOG_FILE_PATH.
+# format="...": This defines the format for log messages, including:
+    # %(asctime)s: The timestamp when the log entry was created.
+    # %(lineno)d: The line number where the log entry was generated.
+    # %(name)s: The name of the logger or module.
+    # %(levelname)s: The severity level of the log (INFO, WARNING, ERROR, etc.).
+    # %(message)s: The actual log message.
+# level=logging.INFO: This sets the logging level to INFO, meaning only log messages with this level or higher will be recorded (e.g., INFO, WARNING, ERROR).
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
 )
 
+
+# This script configures logging to write messages to a dynamically generated log file. 
+# The log file is stored in a "logs" directory, 
+# and the format of the log messages includes important details like timestamps 
+# and the severity level of the messages.
