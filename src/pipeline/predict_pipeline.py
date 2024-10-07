ff0c0145d76e199b93d74cdfcd96b44ba57d1fcf
Kamau <kevinnjagi83@gmail.com>
2024-10-07 15:41:02 +0300

    aws beanstalk deployment

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -4,8 +4,9 @@ from src.exception import CustomException
 from src.utils import load_object # to load our pkl file
 
 
+# Class for loading a model, preprocessing input data, and making predictions
 class PredictPipeline:
-    def __init__(self):
+    def __init__(self): # initialize the class without any specific attributes
         pass
 
     def predict(self,features):
@@ -13,6 +14,7 @@ class PredictPipeline:
             # Load the trained model and preprocessor
             model_path = 'artifacts/model.pkl'
             preprocessor_path = 'artifacts/preprocessor.pkl'
+
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
             
@@ -27,8 +29,9 @@ class PredictPipeline:
             raise CustomException (e,sys)
         
 
+# Class to structure input data in a way that can be easily converted to a DataFrame for the prediction pipeline
 class CustomData:
-    def __init__(  self,
+    def __init__(  self, # initializes a CustomData object with attributes for the model's input features
         gender: str,
         race_ethnicity: str,
         parental_level_of_education,
@@ -52,7 +55,7 @@ class CustomData:
         self.writing_score = writing_score
 
     
-    def get_data_as_data_frame(self):
+    def get_data_as_data_frame(self): # This method creates a DataFrame from the attributes of the CustomData object 
         try:
             # Prepare the data in a dictionary form and convert to pandas DataFrame
             custom_data_input_dict = {
@@ -65,7 +68,7 @@ class CustomData:
                 "writing_score": [self.writing_score],
             }
 
-            return pd.DataFrame(custom_data_input_dict)
+            return pd.DataFrame(custom_data_input_dict) # Converts the dictionary to a DataFrame and returns it.
 
         except Exception as e:
             raise CustomException(e, sys)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 import os
-import sys # for exception handling
+import sys  # for exception handling
 import numpy as np
 import pandas as pd
 import dill
@@ -7,65 +7,61 @@ from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
-import pickle
+
 
 def load_object(file_path):
-    with open(file_path, 'rb') as file:
-        return pickle.load(file)
+    """Load a serialized object from a file using dill."""
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
+        raise CustomException(e, sys)
+
 
 def save_object(file_path, obj):
+    """Save a serialized object to a file using dill."""
     try:
-        dir_path=os.path.dirname(file_path)
-
+        dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
 
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)
-    
+        raise CustomException(e, sys)
 
 
-def evaluate_models(X_train,y_train, X_test, y_test,models, param):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    """Evaluate multiple models using GridSearchCV for hyperparameter tuning."""
     try:
-        report={}
+        report = {}
 
-        # going through each model
-        for i in range(len(list(models))):
-            model=list(models.values())[i]
-            
-            # hyperparameter tuning
-            parameter=param[list(models.keys())[i]]
-            gs=GridSearchCV(model,parameter,cv=3)
-            gs.fit(X_train,y_train)
+        # Iterate through each model
+        for i in range(len(models)):
+            model = list(models.values())[i]
 
+            # Hyperparameter tuning
+            parameter = param[list(models.keys())[i]]
+            gs = GridSearchCV(model, parameter, cv=3)
+            gs.fit(X_train, y_train)
+
+            # Set best parameters and fit the model
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
-            
-            #model.fit(X_train, y_train)  # train the model
+            model.fit(X_train, y_train)
 
-            # predictions on x_train and y_train
-            y_train_pred=model.predict(X_train)
-            y_test_pred=model.predict(X_test)
+            # Predictions on X_train and X_test
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
 
-            # computing r2_score
-            train_model_score=r2_score(y_train, y_train_pred)
-            test_model_score=r2_score(y_test,y_test_pred)
-            
-            # we keep on appending in this report 
-            report[list(models.keys())[i]]=test_model_score
+            # Compute r2_score
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
 
-            return report
-    
-    except Exception as e:
-        raise CustomException (e,sys)
-    
-
-def load_object(file_path):
-    try:
-        with open(file_path, "rb") as file_obj:
-            return dill.load(file_obj)
+            # Append results to the report
+            report[list(models.keys())[i]] = test_model_score
         
+        return report
+
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
